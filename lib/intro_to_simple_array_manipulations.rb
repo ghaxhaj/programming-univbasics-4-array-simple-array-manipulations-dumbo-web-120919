5696b1399c45419f03e05416acb5e005193bb700
wise-integer-6834 <ghaxhaj@fordham.edu>
2019-12-01 01:45:16 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,9 @@
+artists = ["Hozier", "Ariana Grande", "Usher"]
+next_artist = "Beyonce"
+artists.push(next_artist)
+p artists
+
+updated_array = using_push(colors_in_the_rainbow, next_color)
+
+a = [b,c,d]
+a.unshift("a")
