e9cc23d20d2e7fd9b9784edfa1fa436c68932f38
wise-integer-6834 <ghaxhaj@fordham.edu>
2019-12-01 01:46:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,47 @@
-artists = ["Hozier", "Ariana Grande", "Usher"]
-next_artist = "Beyonce"
-artists.push(next_artist)
-p artists
+def using_push(array, string)
+  array.push(string)
+end
 
-updated_array = using_push(colors_in_the_rainbow, next_color)
+def using_unshift(array, string)
+  array.unshift(string)
+end
 
-a = [b,c,d]
-a.unshift("a")
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, item)
+  array.insert(4, item)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, num)
+  array.delete_at(num)
+end
